9481bbf94e49a1c1d9697f30c076a6d3dbe6a3b5
Saidahmad177 <saidahmad177@mail.ru>
2023-05-08 18:33:26 +0500

    database

--- ads/models.py
+++ ads/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Ad(models.Model):
-    pass
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100)
+    price = models.CharField(max_length=18)
+    description = models.TextField(max_length=9000)
+    created_time = models.DateTimeField(auto_now=timezone.now)
+
+
+class Image(models.Model):
+    ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='ads/', defaul='default_img.jpg')
 
 
 class Category(models.Model):
--- olx/settings.py
+++ olx/settings.py
@@ -7,7 +7,7 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(BASE_DIR, '.env')
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -17,8 +17,7 @@ environ.Env.read_env(BASE_DIR, '.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = env('DEBUG')
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,6 +110,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static-files'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
