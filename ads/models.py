5e78d747445ac134f893a1a2c83821aad7db2af0
Saidahmad177 <saidahmad177@mail.ru>
2023-05-09 18:06:32 +0500

    complate models

--- ads/models.py
+++ ads/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class CategoryCity(models.Model):
+    name = models.CharField(max_length=50)
 
 
 class Ad(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100)
+    title = models.CharField(max_length=200, null=False)
+    # slug = models.SlugField(max_length=100)
+    slug = slugify(title)
     price = models.CharField(max_length=18)
     description = models.TextField(max_length=9000)
     created_time = models.DateTimeField(auto_now=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category_city = models.ForeignKey(CategoryCity, on_delete=models.CASCADE)
 
 
 class Image(models.Model):
     ad_id = models.ForeignKey(Ad, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='ads/', defaul='default_img.jpg')
+    image = models.ImageField(upload_to='ads/', default='default_img.jpg')
 
-
-class Category(models.Model):
-    pass
-
-
-class CategoryCity(models.Model):
-    pass
--- olx/settings.py
+++ olx/settings.py
@@ -31,6 +31,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external app
+    'ads',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -105,13 +109,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static-files'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
